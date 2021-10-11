15fae631d5e475ea486e2f6849550359725dd6ad
Saroj  Patnaik <Saroj.Patnaik@gds.ey.com>
2021-10-11 21:53:53 +0530

    Added MemberShip and UpgradeMemberShip Implementations

---
+++ OrderProcessingSystem/Classes/MemberShip.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderProcessingSystem.Classes
+{
+    public class MemberShip:Product
+    {
+        public MemberShip(string productName, double price, string ownerName) : base(productName, price, ownerName)
+        {
+        }
+
+        public override bool DoPayment()
+        {
+           Console.WriteLine("MemberShip Payment Completed");
+           return true;
+        }
+    }
+}
---
+++ OrderProcessingSystem/Classes/UpgradeMemberShip.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderProcessingSystem.Classes
+{
+    public class UpgradeMemberShip:MemberShip
+    {
+        public UpgradeMemberShip(string productName, double price, string ownerName) : base(productName, price, ownerName)
+        {
+        }
+        public override bool DoPayment()
+        {
+            Console.WriteLine("UpgradeMemberShip Payment Completed");
+            return true;
+        }
+    }
+}

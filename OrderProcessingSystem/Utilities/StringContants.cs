377ed099b7d1753feffe5a644ac9354e21ea18bf
Saroj  Patnaik <Saroj.Patnaik@gds.ey.com>
2021-10-11 22:26:05 +0530

    Added Constants File for Configurations

---
+++ OrderProcessingSystem/Utilities/StringContants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderProcessingSystem.Utilities
+{
+    static class  StringConstants
+    {
+        private static double AgentCommission = 30;
+    }
+}
